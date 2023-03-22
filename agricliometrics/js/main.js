e7b7b421de56e54ad40dab5d621233879adaf312
Matias2302 <matias.23.02@gmail.com>
2023-03-22 11:17:27 -0300

    version en español

---
+++ agricliometrics/js/main.js
@@ -0,0 +1,10 @@
+/*=====NAVBAR COLOR=====*/
+
+$(function () {
+	$(document).scroll(function () {
+	  var $nav = $("header");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+  });
+
+/*======================*/
